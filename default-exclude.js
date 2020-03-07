0bec51b8dff80e626a98d078293ec4581022d54b
Corey Farrell <git@cfware.com>
2020-03-07 08:12:51 -0500

    fix: Exclude tests with `tsx` or `jsx` extensions
    Fixes #11

--- default-exclude.js
+++ default-exclude.js
@@ -1,12 +1,17 @@
 'use strict';
 
+const defaultExtension = require('./default-extension.js');
+const testFileExtensions = defaultExtension
+	.map(extension => extension.slice(1))
+	.join(',');
+
 module.exports = [
 	'coverage/**',
 	'packages/*/test{,s}/**',
 	'**/*.d.ts',
 	'test{,s}/**',
-	'test{,-*}.{js,cjs,mjs,ts}',
-	'**/*{.,-}test.{js,cjs,mjs,ts}',
+	`test{,-*}.{${testFileExtensions}}`,
+	`**/*{.,-}test.{${testFileExtensions}}`,
 	'**/__tests__/**',
 
 	/* Exclude common development tool configuration files */
